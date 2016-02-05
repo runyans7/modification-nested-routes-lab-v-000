76cea7a1e271b93fbb3c57fe571e871947d9bfd7
Scotty Runyan <runyans7@gmail.com>
2016-02-05 09:07:12 -0500

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,5 +1,14 @@
+require 'pry'
 module ArtistsHelper
   def display_artist(song)
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
+  end
+
+  def artist_select(is_nested, song)
+    if is_nested
+     song.artist.name
+    else
+      select_tag 'song[artist_id]', options_from_collection_for_select(Artist.all, :id, :name)
+    end
   end
 end
